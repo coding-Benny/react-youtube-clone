29f639aa7081b50fdbdd055cc3aa2ce9f03b83ff
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-08-23 15:17:52 +0900

    create video detail page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -30,14 +30,15 @@ function LandingPage() {
         var seconds = Math.floor((video.duration - minutes * 60));
 
         return <Col lg={ 6 } md={ 8 } xs={ 24 }>
-        <a href={'/video/post/${video._id}'} >
+        
         <div style={{ position: 'relative' }}>
-            <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
-            <div className="duration">
-                <span>{ minutes } : { seconds }</span>
-            </div>
+            <a href={`/video/${video._id}`} >
+                <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                <div className="duration">
+                    <span>{ minutes } : { seconds }</span>
+                </div>
+            </a>
         </div>
-        </a>
         <br />
         <Meta
             avatar={
