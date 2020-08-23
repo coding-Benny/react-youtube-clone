8947465389eee719ae44a176b3f76b186a305dc0
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-08-23 15:48:27 +0900

    create side video at video detail page

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 
 function VideoDetailPage(props) {
 
@@ -40,7 +41,7 @@ function VideoDetailPage(props) {
                     </div>
                 </Col>
                 <Col lg={ 6 } xs={ 24 }>
-                    Side Videos
+                    <SideVideo />
                 </Col>
             </Row>
         )
